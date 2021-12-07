dd68832de35cbf61bf7aa70288be0d53b1c68453
Gustavo França <gustavocandidofranca@gmail.com>
2021-12-07 20:25:54 -0300

    Add eslint

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './components/layout';
+import Router from './routes';
+
+
+const App = ()=> (
+	<Layout>
+		<Router />
+	</Layout>
+);
+
+export default App;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-import { BrowserRouter , Route, Routes } from "react-router-dom";
-import ProductList from "../pages/productList";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProductList from '../pages/productList';
 
-const Router = ()=> {
-    return (
-        <BrowserRouter>
-                <Routes >
-                    <Route path="/" element={<ProductList/>} />
-                </Routes>
-        </BrowserRouter>
-    );
-}
+const Router = () => (
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<ProductList/>}/>
+		</Routes>
+	</BrowserRouter>
+);
 export default Router;
