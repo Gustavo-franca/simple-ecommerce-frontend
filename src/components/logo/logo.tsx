0935224aa9360c846caf2959c2e0e4dbf2d94966
Gustavo França <gustavocandidofranca@gmail.com>
2022-02-01 20:12:19 -0300

    Add link to logo

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { LogoStyled } from './styles';
 import { useApplicationContext } from '../../contexts/application';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
   const {
     config: { appName }
   } = useApplicationContext();
 
-  return <LogoStyled>{appName}</LogoStyled>;
+  return (
+    <Link to={{ pathname: '/' }}>
+      <LogoStyled>{appName}</LogoStyled>
+    </Link>
+  );
 };
 export default Logo;
